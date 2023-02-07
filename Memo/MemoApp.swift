1853252d72be2dce96865461ccb329bfb2dac2b5
seidaiegoshi <egoshi.seidai@gmail.com>
2023-02-07 18:12:08 +0900

    Initial Commit

---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by 江越正大 on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
